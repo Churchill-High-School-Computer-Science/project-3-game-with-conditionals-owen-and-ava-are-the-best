68253a4a14d15c79e5146c46aaa07690d7c9f117
Ava Medina <amedina0643@stu.neisd.net>
2024-10-09 13:05:06 -0500

    db

--- wordle.java
+++ wordle.java
@@ -2,30 +2,38 @@ import java.util.Scanner;
 
 public class wordle {
     public static void main(String[] args) {
-    final String green = "\u001B[32m";
-    final String yellow = "\u001B[33m";
-    final String black = "\u001B[30m";
+    final String green = "\u001b[32m";
+    final String yellow = "\u001b[33m";
+    final String reset = "\u001b[0m";
     String listofwords[] = {"hello", "world", "yummy", "dummy", "coder", "doggy", "yucky", "crane", "grand", "debug"};
     String targetWord = listofwords[(int)(Math.random() * 10)];
         Scanner input = new Scanner(System.in); 
     System.out.println("Enter a random word with 5 letters:"); 
     String potentialword = input.next();
-    if(potentialword.substring(0, 1).equals(targetWord.substring(0, 1)))
-    {   //letter matches
-        System.out.println(green + potentialword.substring(0, 1) + green);
-    }
-    else if(targetWord.indexOf(potentialword.substring(0,1)) > -1)
-    {   // letter in diff place 
-        System.out.println(yellow + potentialword.substring(0, 1) + yellow);
-    }
-    else if(potentialword.substring(5).equals(targetWord))
+    for(int i=0;i<5;i++)
     {
-        System.out.println("Game over! The word was " + targetWord);
+
+    if(potentialword.substring(i, i+1).equals(targetWord.substring(i, i+1)))
+    {   //letter matches
+        System.out.println(green + potentialword.substring(i, i+1) + reset);
+    }
+    else if(targetWord.indexOf(potentialword.substring(i, i+1)) > -1)
+    {   // letter in diff place 
+        System.out.println(yellow + potentialword.substring(i, i+1) + reset);
     }
     else
     {
-        System.out.println(black + potentialword.substring(0, 1));
+        System.out.println(potentialword.substring(i, i+1));
+    }
+    System.out.println("");
+    if(potentialword.equals(targetWord))
+    {
+        System.out.println("Correct!");
+    }
+    if(!potentialword.equals(targetWord))
+    {
+        System.out.println("Wrong!");
     }
     }
-   
+}
 }
