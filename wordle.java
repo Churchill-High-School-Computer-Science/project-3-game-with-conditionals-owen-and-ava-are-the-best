3170ac2bbf4f901dca1cb5ed12323c80858be538
Ava Medina <amedina0643@stu.neisd.net>
2024-10-09 12:42:06 -0500

    dgddb

--- wordle.java
+++ wordle.java
@@ -2,32 +2,30 @@ import java.util.Scanner;
 
 public class wordle {
     public static void main(String[] args) {
-    final String green = "\u001b[421m"
-    final String yellow = "\u001b[43m"
-    final String clear = "\u001b[0m"
+    final String green = "\u001B[32m";
+    final String yellow = "\u001B[33m";
+    final String black = "\u001B[30m";
     String listofwords[] = {"hello", "world", "yummy", "dummy", "coder", "doggy", "yucky", "crane", "grand", "debug"};
     String targetWord = listofwords[(int)(Math.random() * 10)];
-    Integer.MAX_VALUE = 6;
         Scanner input = new Scanner(System.in); 
     System.out.println("Enter a random word with 5 letters:"); 
-    String potentialword = input.nextLine();
+    String potentialword = input.next();
     if(potentialword.substring(0, 1).equals(targetWord.substring(0, 1)))
-    {
-        System.out.println(green + potentialword.substring(0, 1) + clear);
+    {   //letter matches
+        System.out.println(green + potentialword.substring(0, 1) + black);
     }
-    else if(targetWord.indexOf(potentialword.substring(0,1) > -1))
-    {
-        System.out.println(green + potentialword.substring(0, 1) + clear);
+    else if(targetWord.indexOf(potentialword.substring(0,1)) > -1)
+    {   // letter in diff place 
+        System.out.println(green + potentialword.substring(0, 1) + yellow);
     }
     else if(potentialword.substring(5).equals(targetWord))
     {
         System.out.println("Game over! The word was " + targetWord);
     }
-    else if(MAX_VALUE.less(potentialword))
+    else
     {
-        System.out.println("Too many words! Enter another random word with 5 letters");
+        System.out.println(black + potentialword.substring(0, 1));
     }
-
     }
    
 }
